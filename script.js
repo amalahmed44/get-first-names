2aedb2e2ecda69ddfb6cacb36bf8802d37f602a4
amalali8 <amalza958@gmail.com>
2023-08-10 18:04:32 +0300

    answered

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-// Good Luck 💪🏾
+function getFirstNames(names) {
+  const firstNames = [];
+
+  for (let i = 0; i < names.length; i++) {
+    const innerArray = names[i];
+    if (Array.isArray(innerArray) && innerArray.length > 0) {
+      const firstName = innerArray[0];
+      firstNames.push(firstName);
+    }
+  }
+
+  return firstNames;
+}
+
+const namesArray = [["John", "Doe"], ["Jane", "Smith"], ["Michael", "Johnson"]];
+console.log(getFirstNames(namesArray)); 
